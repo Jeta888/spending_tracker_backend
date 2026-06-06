d087df42a2eecb23896aa9bd7e7b33554738e1ca
Jeta Spahiu <jetaspahiu04@gmail.com>
2026-06-06 12:50:40 +0200

    Use expense routes

--- src/routes/expenseRoutes.js
+++ src/routes/expenseRoutes.js
@@ -4,6 +4,9 @@ const authMiddleware = require("../middleware/authMiddleware");
 const {
   createExpense,
   getExpenses,
+  getExpenseById,
+  updateExpense,
+  deleteExpense,
   getMonthlySummary,
   getWeeklySummary,
 } = require("../controllers/expenseController");
@@ -14,7 +17,12 @@ router.use(authMiddleware);
 
 router.post("/", createExpense);
 router.get("/", getExpenses);
+
 router.get("/summary/monthly", getMonthlySummary);
 router.get("/summary/weekly", getWeeklySummary);
+
+router.get("/:id", getExpenseById);
+router.put("/:id", updateExpense);
+router.delete("/:id", deleteExpense);
 
 module.exports = router;
