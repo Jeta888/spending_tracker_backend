f5b7bc60aa1538d9f96e63d65dc9af0062416582
Jeta Spahiu <jetaspahiu04@gmail.com>
2026-06-06 10:30:46 +0200

    Use express app, configure db and server

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Spending Tracker API is running",
+  });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/expenses", expenseRoutes);
+
+module.exports = app;
